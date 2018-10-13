effd9ccb306e23bc3f4a46b1bb3a415be73347dd
Dranslin <dranslin@gmail.com>
2018-10-13 13:48:48 -0700

    Update

--- C2A3/C2A3E2/C2A3E2_TestDeclarations.cpp
+++ C2A3/C2A3E2/C2A3E2_TestDeclarations.cpp
@@ -1,22 +1,22 @@
 /*
-* Nicholas Patience U07985691
-* Nickspatience@gmail.com
-* C/C++ Programming II : Dynamic Memory and File I/O Concepts
-* 134312 Ray Mitchell
-* 10/12/2018
-* C2A3E2_TestDeclarations.cpp
-* Win 10
-* Visual C++ 2017
-*
-* Creates declaractions for the following cases
-* 1. Declares cHelp to be a function returning a reference to a char that has
-* a parameter named help thats a reference to a float.
-* 2. Declares abc to be a pointer to an array of 9 pointers to shorts.
-* 3. Declares def to be a reference to a pointer to an array of 9 pointers to shorts.
-* 4. Declares dpA to be an array of 9 pointers to doubles.
-* 5. Type casts(C - style) dpA to a pointer to an array of 9 pointers to shorts
-* and assigns the result to abc.
-*/
+ * Nicholas Patience U07985691
+ * Nickspatience@gmail.com
+ * C/C++ Programming II : Dynamic Memory and File I/O Concepts
+ * 134312 Ray Mitchell
+ * 10/12/2018
+ * C2A3E2_TestDeclarations.cpp
+ * Win 10
+ * Visual C++ 2017
+ *
+ * Creates declaractions for the following cases
+ * 1. Declares cHelp to be a function returning a reference to a char that has
+ * a parameter named help thats a reference to a float.
+ * 2. Declares abc to be a pointer to an array of 9 pointers to shorts.
+ * 3. Declares def to be a reference to a pointer to an array of 9 pointers to shorts.
+ * 4. Declares dpA to be an array of 9 pointers to doubles.
+ * 5. Type casts(C - style) dpA to a pointer to an array of 9 pointers to shorts
+ * and assigns the result to abc.
+ */
 
 const int ARRAYVALUE = 9;
 
