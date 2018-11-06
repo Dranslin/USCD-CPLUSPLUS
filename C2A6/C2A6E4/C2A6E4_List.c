8054aa4a47651c9994a683048a0b07afdc11b094
Dranslin <dranslin@gmail.com>
2018-11-06 13:52:20 -0800

    Updating.

--- C2A6/C2A6E4/C2A6E4_List.c
+++ C2A6/C2A6E4/C2A6E4_List.c
@@ -8,7 +8,10 @@
 * Win 10
 * Visual C++ 2017
 *
-* 
+* Provides definitions for CreateNode, CreateList, PrintList and FreeList.
+* These functions dynamically allocate space for new nodes and each node's
+* string, tests provided strings then fills in the node's strings and counts
+* for each string, prints out the results, and frees the allocated memory.
 */
 
 #include "C2A6E4_List-Driver.h"
@@ -17,6 +20,35 @@
 
 #define MAX_BUFFER_SIZE 128
 
+List *CreateNode(const char *targetString);
+
+// Allocate space for each node and node's string, then return pointer to node
+List *CreateNode(const char *targetString)
+{
+    List *newNode = (struct List *)malloc(sizeof(struct List));
+
+    // Testing for failure
+    if (newNode == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize values.
+    newNode->next = NULL;
+    newNode->count = 1;
+
+    // Allocate space for string, then copy.
+    //newNode->str = (char *)malloc(sizeof(char) * (sizeof(targetString) / sizeof(targetString)[0]));
+    newNode->str = (char *)malloc((sizeof(char) * (strlen(targetString) + 1)));
+    strcpy(newNode->str, targetString);
+
+    return newNode;
+}
+
+// Creates the linked list, testing for matching strings in linked list
+// increments counts for each matched string to a node, and creates nodes
+// if not found.
 List *CreateList(FILE *fp)
 {
     List *head = NULL, *walker = NULL;
@@ -36,18 +68,21 @@ List *CreateList(FILE *fp)
 
         // Traverse list: check for string, then check for next node, 
         // then create if not there.
-        printf("Strcmp returns %d\n", strcmp(walker->str, readString));
+        //printf("Strcmp returns %d\n", strcmp(walker->str, readString));
         while (walker != NULL)
         {
+            // If match found, increment count and stop traversing.
             if (strcmp(walker->str, readString) == 0)
             {
                 walker->count += 1;
-                walker = walker->next;
+                walker = NULL;
             }
+            // Continue walking list.
             else if (walker->next != NULL)
             {
                 walker = walker->next;
             }
+            // If at end of list and no match, create new node.
             else
             {
                 walker->next = CreateNode(readString);
@@ -58,45 +93,31 @@ List *CreateList(FILE *fp)
     return head;
 }
 
-List *CreateNode(const char *targetString)
-{
-    List *newNode = (struct List *)malloc(sizeof(struct List));
-
-    if (newNode == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
-    newNode->next = NULL;
-    newNode->count = 0;
-
-    newNode->str = (char *)malloc((sizeof(char) * strlen(targetString)));
-    strcpy(newNode->str, targetString);
-
-    return newNode;
-}
-
+// Traverses the list and prints out the string and count for each string.
 List *PrintList(const List *head)
 {
     List *walker = head;
 
+    // Traverse list and print values
     while (walker != NULL)
     {
-        printf("%s %d4 ea\n", walker->str, walker->count);
+        printf("%s %4d ea\n", walker->str, walker->count);
         walker = walker->next;
     }
     return head;
 }
 
+// Free two malloc cases, node and node's string
 void FreeList(List *head)
 {
     List *walker = head, *temp;
 
+    // Traverse through the list
     while (walker->next != NULL)
     {
         temp = walker;
         walker = walker->next;
-
+        // String must be freed first
         free(temp->str);
         free(temp);
     }
